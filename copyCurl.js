3ee0217b2575f5cb456beede810398db1cec02e1
Frank Bellotto <frank@monpetitgazon.com>
2022-04-04 23:07:20 +0200

    release 1.0

---
+++ copyCurl.js
@@ -0,0 +1,48 @@
+const translate = (text) => {
+  const lines = text.split('\n').filter(Boolean);
+
+  const [method, uri] = lines.shift().split(' ');
+  const attributes = {
+    method,
+    headers: {},
+  };
+
+  while (lines.length) {
+    const line = lines.shift();
+    const [key, value] = line.replace(/\r/g, '').split(': ');
+    if (key === 'Host') {
+      attributes.url = `https://${value}${uri}`;
+    } else if (key === 'User-Agent') {
+      attributes.userAgent = value;
+    } else if (key === 'Content-Length') {
+      // Nothing to do.
+    } else if (key === 'Accept') {
+      // Nothing to do.
+    } else if (key === 'Accept-Encoding') {
+      // Nothing to do.
+    } else if (key.indexOf('{') === 0) {
+      attributes.data = JSON.parse([line, ...lines].join(' '));
+      lines.length = 0;
+    } else {
+      attributes.headers[key] = value;
+    }
+  }
+
+  return `
+curl \\
+--compressed \\
+${attributes.userAgent ? `--user-agent '${attributes.userAgent}'` : ''} \\
+${Object.keys(attributes.headers).filter(key => attributes.headers[key]).map(key => `--header '${key}: ${attributes.headers[key]}'`).join(' \\\n')} \\
+${attributes.data ? `--data-binary '${JSON.stringify(attributes.data).replace(/'/g, '\'\\\'\'')}'` : ''} \\
+${attributes.url}
+  `;
+};
+
+window.onload = function () {
+  document.querySelector('#request-pane-2').addEventListener('click', async (event) => {
+    const t = document.querySelector('#request-pane-2 > pre > code');
+    const curlCommand = translate(t.innerHTML);
+    await navigator.clipboard.writeText(curlCommand);
+    console.info('Copied.');
+  });
+};
