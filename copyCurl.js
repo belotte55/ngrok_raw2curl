762b1c8821e9affd6431363aab89b290dbf6b866
Frank Bellotto <frank@monpetitgazon.com>
2022-11-14 22:36:50 +0100

    release 2.0

--- copyCurl.js
+++ copyCurl.js
@@ -28,13 +28,13 @@ const translate = (text) => {
     }
   }
 
-  return `
+  return `\
 curl \\
 --compressed \\
 ${attributes.userAgent ? `--user-agent '${attributes.userAgent}'` : ''} \\
 ${Object.keys(attributes.headers).filter(key => attributes.headers[key]).map(key => `--header '${key}: ${attributes.headers[key]}'`).join(' \\\n')} \\
 ${attributes.data ? `--data-binary '${JSON.stringify(attributes.data).replace(/'/g, '\'\\\'\'')}'` : ''} \\
-${attributes.url}
+'${attributes.url}'\
   `;
 };
 
@@ -43,9 +43,33 @@ const copyCurl = async () => {
   const curlCommand = translate(element.innerHTML);
   await navigator.clipboard.writeText(curlCommand);
   console.info('Copied.');
+
+  Toastify(toastConfig).showToast();
 };
 
-window.onload = () => {
-  document.querySelector('#request-pane-2').addEventListener('click', async () => copyCurl());
-  document.querySelector('#app > div > div > div > div > div.styles__inspectPage__3rnNV > div.styles__section__3dDb2.styles__selector__3hiW9 > div > table > tbody').addEventListener('dblclick', async () => copyCurl());
+window.onload = async () => {
+  document.querySelector('#request-pane-2').addEventListener('click', copyCurl);
+  document.querySelector('#app > div > div > div > div > div > div > div > table > tbody').addEventListener('dblclick', copyCurl);
 };
+
+;(() => {
+  const headerButton = document.createElement('a')
+  headerButton.appendChild(document.createTextNode('Copy cURL'));
+  headerButton.href = '#'
+  const headerList = document.createElement('li')
+  headerList.appendChild(headerButton)
+  headerList.addEventListener('click', copyCurl);
+  document.querySelector('#app > nav > div > div.navbar-collapse.collapse > ul.nav.navbar-nav.navbar-right').appendChild(headerList);
+
+  const buttons = document.querySelector('#app > div > div > div > div > div > div:nth-child(2) > div > div:nth-child(3) > div > div.pull-right > div > div')
+
+  const button = document.createElement('button')
+  button.type = 'button'
+  button.classList.add(...['foo', 'btn', 'btn-primary'])
+  button.innerHTML = 'cURL'
+  button.addEventListener('click', copyCurl)
+  buttons.prepend(button)
+
+  // Remove unused Binary tab.
+  document.querySelector('#request > ul > li:nth-child(4)').remove()
+})();
